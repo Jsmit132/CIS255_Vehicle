c556e101cd5e34349e49363232cc1b4a11d74817
Jsmit132 <Jsmit132@jeffersonstate.edu>
2023-07-26 18:49:56 -0500

    Class - Make #30 - Revised Make Class
    Class - Design #31 - Revised Code to better separate purposes

--- src/CIS255_Vehicle.java
+++ src/CIS255_Vehicle.java
@@ -18,7 +18,7 @@ public class CIS255_Vehicle {
             // Menu Switch
          switch (navMain) {
             case "1": // Show create vehicle menu
-               Make.createMakeMenu(navScan, vehicleStorage);
+               createMakeMenu(navScan, vehicleStorage);
                break;
             case "2": // Show created vehicles
                vehicleStorage.showVehiclesMenu();
@@ -60,11 +60,82 @@ public class CIS255_Vehicle {
       System.out.print("Enter your choice: ");
    }
 
+   public static void createMakeMenu(Scanner navScan, VehicleStorage vehicleStorage) {
+      String navMake;
    
+      do {   
+         displayMakeMenu();
+         navMake = navScan.nextLine();
+      
+         switch (navMake) {
+            case "1": // Dodge
+               Dodge.createVehicle(navScan, "Dodge", vehicleStorage);
+               break;
+         
+            case "2": // Ford
+               Ford.createVehicle(navScan, "Ford", vehicleStorage);
+               break;
+         
+            case "3": // Honda
+               Honda.createVehicle(navScan, "Honda", vehicleStorage);
+               break;
+         
+            case "4": // Tesla
+               Tesla.createVehicle(navScan, "Tesla", vehicleStorage);
+               break;
+         
+            case "5": // Toyota
+               Toyota.createVehicle(navScan, "Toyota", vehicleStorage);
+               break;
+         
+            case "6":  // Break to return to the previous menu
+               return;
+         
+            default: // Error
+               System.out.println("Invalid choice. Please try again.");
+         }
+      } while (!navMake.equals("6"));
+   }
 
-   private static void showVehiclesMenu(Scanner navScan) {
-        // Logic for showing vehicles
-      System.out.println("Showing vehicles...");
+
+    // Display create vehicle menu
+   private static void displayMakeMenu() {
+      System.out.println("\n");
+      System.out.println("=== Select A Make === \n");
+   
+      System.out.println("1. Create Dodge");
+      System.out.println("2. Create Ford");
+      System.out.println("3. Create Honda");
+      System.out.println("4. Create Tesla");
+      System.out.println("5. Create Toyota");
+      System.out.println("6. Go Back \n");
+   
+      System.out.print("Enter your choice: ");
+   }
+   
+   public static void displayTypeMenu(String make) {
+   
+      if (make.equalsIgnoreCase("tesla")) {
+      
+         System.out.println("\n");
+         System.out.println("=== Select a type for " + make +" === \n");
+         System.out.println("1. Create Sedan");
+         System.out.println("2. Go Back \n");
+      
+         System.out.print("Enter your choice: ");
+         
+      }else{
+      
+         System.out.println("\n");
+         System.out.println("=== Select a type for " + make +" === \n");
+      
+         System.out.println("1. Create Coupe");
+         System.out.println("2. Create Sedan");
+         System.out.println("3. Create Truck");
+         System.out.println("4. Go Back \n");
+      
+         System.out.print("Enter your choice: ");
+      }
    }
 
 }
--- src/Classes/Make.java
+++ src/Classes/Make.java
@@ -1,81 +1,10 @@
 import java.util.Scanner;
 
 public abstract class Make extends CIS255_Vehicle{
-   public static void createMakeMenu(Scanner navScan, VehicleStorage vehicleStorage) {
-      String navMake;
-   
-      do {   
-         displayMakeMenu();
-         navMake = navScan.nextLine();
-      
-         switch (navMake) {
-            case "1": // Dodge
-               Dodge.createVehicle(navScan, "Dodge", vehicleStorage);
-               break;
-         
-            case "2": // Ford
-                    Ford.createVehicle(navScan, "Ford", vehicleStorage);
-               break;
-         
-            case "3": // Honda
-                    Honda.createVehicle(navScan, "Honda", vehicleStorage);
-               break;
-         
-            case "4": // Tesla
-                    Tesla.createVehicle(navScan, "Tesla", vehicleStorage);
-               break;
-         
-            case "5": // Toyota
-                    Toyota.createVehicle(navScan, "Toyota", vehicleStorage);
-               break;
-         
-            case "6":  // Break to return to the previous menu
-               return;
-         
-            default: // Error
-               System.out.println("Invalid choice. Please try again.");
-         }
-      } while (!navMake.equals("6"));
+   public void displayMakeInfo() {
+      // Display the make menu for the specific make
    }
-
-
-    // Display create vehicle menu
-   private static void displayMakeMenu() {
-      System.out.println("\n");
-      System.out.println("=== Select A Make === \n");
-   
-      System.out.println("1. Create Dodge");
-      System.out.println("2. Create Ford");
-      System.out.println("3. Create Honda");
-      System.out.println("4. Create Tesla");
-      System.out.println("5. Create Toyota");
-      System.out.println("6. Go Back \n");
-   
-      System.out.print("Enter your choice: ");
+   public void displayModelMenu() {
+      // Display the model menu for the specific make
    }
-   
-   public static void displayTypeMenu(String make) {
-   
-      if (make.equalsIgnoreCase("tesla")) {
-      
-         System.out.println("\n");
-         System.out.println("=== Select a type for " + make +" === \n");
-         System.out.println("1. Create Sedan");
-         System.out.println("2. Go Back \n");
-      
-         System.out.print("Enter your choice: ");
-         
-      }else{
-      
-         System.out.println("\n");
-         System.out.println("=== Select a type for " + make +" === \n");
-      
-         System.out.println("1. Create Coupe");
-         System.out.println("2. Create Sedan");
-         System.out.println("3. Create Truck");
-         System.out.println("4. Go Back \n");
-      
-         System.out.print("Enter your choice: ");
-      }
-   } 
 }
